81665cb4fed64a44913685115c98d028b4428d52
bilonhdo-star <bilonhdo@gmail.com>
2026-05-09 03:11:41 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const socket = io("YOUR_RENDER_URL");
+
+const chat = document.getElementById("chat");
+const status = document.getElementById("status");
+
+socket.on("connect", () => {
+  status.innerText = "Connected. Waiting for partner...";
+});
+
+socket.on("matched", () => {
+  status.innerText = "Matched with stranger!";
+});
+
+socket.on("message", (msg) => {
+  chat.innerHTML += "<p>" + msg + "</p>";
+});
+
+socket.on("partner-left", () => {
+  status.innerText = "Partner left. Waiting...";
+});
+
+function send() {
+  const msg = document.getElementById("msg").value;
+  socket.emit("message", msg);
+  chat.innerHTML += "<p>You: " + msg + "</p>";
+}
+
+function sendEmoji(e) {
+  socket.emit("message", e);
+  chat.innerHTML += "<p>You: " + e + "</p>";
+}
+
+function nextUser() {
+  socket.emit("next");
+  chat.innerHTML = "";
+}
